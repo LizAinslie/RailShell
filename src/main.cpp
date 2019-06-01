5ccd9627b86bd41278bb626b01981508c0cee812
RailRunner16 <railinator4903@gmail.com>
2019-06-01 02:06:39 +0000

    author

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,52 @@
 #include <iostream>
 #include <string>
 
+#include "colors.h"
+
+void prefix_out() {
+	std::cout << Color::FG_GREEN << "# " << Color::FG_DEFAULT;
+}
+
 void shutdown(int s) {
-    std::cout << "Quitting..." << std::endl;
-    exit(0);
+	prefix_out();
+	
+	std::cout
+		<< Color::FG_RED
+		<< "Quitting..."
+		<< Color::FG_DEFAULT
+		<< std::endl;
+	
+	exit(0);
 }
 
 int main() {
-    struct sigaction sigIntHandler;
-    
-    sigIntHandler.sa_handler = shutdown;
-    sigemptyset(&sigIntHandler.sa_mask);
-    sigIntHandler.sa_flags = 0;
+	struct sigaction sigIntHandler;
+	
+	sigIntHandler.sa_handler = shutdown;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
 
-    sigaction(SIGINT, &sigIntHandler, NULL);
+	sigaction(SIGINT, &sigIntHandler, NULL);
 
-    std::cout << "Welcome to RailShell!" << std::endl;
-    while(1) {
-        std::cout << "> ";
+	prefix_out();
+	std::cout
+		<< Color::FG_BLUE
+		<< "Welcome to RailShell!"
+		<< Color::FG_DEFAULT
+		<< std::endl;
+	
+	while(1) {
+		std::cout
+			<< Color::FG_DARK_GRAY
+			<< "> "
+			<< Color::FG_DEFAULT;
 
-        std::string line;
-        std::getline(std::cin, line);
+		std::string line;
+		std::getline(std::cin, line);
 
-        if (line == "q") {
-            shutdown(0);
-        }
+		if (line == "q") shutdown(0);
 
-        std::cout << line << std::endl;
-    }
+		prefix_out();
+		std::cout << line << std::endl;
+	}
 }
