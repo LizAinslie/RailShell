f8307e9a02ca301abc8b957630350293734ebf14
Landon Gravat <railinator4903@gmail.com>
2019-06-09 16:50:07 -0500

    wew

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,12 +1,13 @@
 if [[ ! -z "$TRAVIS_TAG" ]]; then
   cmake . -DVERSION=$TRAVIS_TAG;
 else
-  cmake .;
+  cmake . -DVERSION=$(git rev-parse --short HEAD);
 fi
 if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
   cmake --build .;
 else
   cmake --build . -- -j3;
 fi
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then cpack -G DEB .; cpack -G RPM .; cpack -G NSIS .; fi
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then cpack -G DEB .; cpack -G RPM .; fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then cpack -G DragNDrop .; fi
+if [[ "$TRAVIS_OS_NAME" == "windows" ]]; then cpack -G NSIS .; fi
